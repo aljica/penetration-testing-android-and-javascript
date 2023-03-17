d98b69965e507ab5c895f93201ac44bf24d48fb9
aljica <aljica@kth.se>
2023-03-17 20:55:19 +0100

    Add code and reports

---
+++ Web-App-Pentest/patched_source_code/forumerly/public/javascript/main.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+
+  // Enable Bootstrap tooltips
+  $(function (){$('[data-toggle="tooltip"]').tooltip({container: 'body'})})
+
+  // Profile image upload
+  $("#profile-img").on('click', () => {
+    $("#file-selector").click()
+  })
+  $("#file-selector").on('change', () => {
+    $('#upload-button').css('display', 'block')
+  })
+
+  $("#fileUpload").on('change', () => {
+    $('#fileNameUpload').html($("#fileUpload").val());
+  })
+
+});
